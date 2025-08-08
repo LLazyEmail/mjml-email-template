1d9997fccd40224f9deb52155b6a5b629ca101f3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2025-08-09 01:19:23 +0300

    Update header.ts

--- typo/header.ts
+++ typo/header.ts
@@ -5,3 +5,17 @@
           <mj-divider border-width="2px" border-color="#616161" width="45%"></mj-divider>
         </mj-column>
       </mj-section>';
+
+
+
+// import { Tag } from "../types";
+
+// const Header: Tag = (attrs, children) => {
+//   const { style, ...restAttrs } = attrs || {};
+//   const defaultStyle = "font-size:24px; margin:20px 0;";
+//   const styleAttr = style ? `${style}; ${defaultStyle}` : defaultStyle;
+
+//   return `<h1 style="${styleAttr}" ${Object.entries(restAttrs || {}).map(([k, v]) => `${k}="${v}"`).join(" ")}>${children}</h1>`;
+// };
+
+// export default Header;
