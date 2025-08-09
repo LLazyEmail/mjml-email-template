09429c88abed8d498e8331225500a4c898601f39
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2025-08-09 10:24:18 +0300

    Update head.ts

--- outer/head.ts
+++ outer/head.ts
@@ -11,7 +11,17 @@
 
 
 
-// ts
+
+
+
+// js
+
+// const Head = (attrs, children) => {
+//   const styleAttr = attrs && attrs.style ? attrs.style : "";
+//   return `<head style="${styleAttr}">${children}</head>`;
+// };
+
+
 
 // import { Tag } from "../types";
 
@@ -19,6 +29,18 @@
 //   const { style } } = attrs || {};
 //   const styleAttr = style ?? "";
 //   return `<head style="${styleAttr}">${children}</head>`;
+// };
+
+// const Head: Tag = (attrs, children) => {
+//   return `<mj-head>
+//       <mj-title>Hello world</mj-title>
+//       <mj-font name="Roboto" href="https://fonts.googleapis.com/css?family=Roboto:300,500"></mj-font>
+//       <mj-attributes>
+//         <mj-all font-family="Roboto, Helvetica, sans-serif"></mj-all>
+//         <mj-text font-weight="300" font-size="16px" color="#616161" line-height="24px"></mj-text>
+//         <mj-section padding="0px"></mj-section>
+//       </mj-attributes>
+//     </mj-head>`;
 // };
 
 // export default Head;
@@ -30,3 +52,19 @@
 //   const styleAttr = attrs && attrs.style ? attrs.style : "";
 //   return `<head style="${styleAttr}">${children}</head>`;
 // };
+
+// const Head = (attrs, children) => {
+//   return `<mj-head>
+//       <mj-title>Hello world</mj-title>
+//       <mj-font name="Roboto" href="https://fonts.googleapis.com/css?family=Roboto:300,500"></mj-font>
+//       <mj-attributes>
+//         <mj-all font-family="Roboto, Helvetica, sans-serif"></mj-all>
+//         <mj-text font-weight="300" font-size="16px" color="#616161" line-height="24px"></mj-text>
+//         <mj-section padding="0px"></mj-section>
+//       </mj-attributes>
+//     </mj-head>`;
+// };
+
+// const result = Head({ anything: 'value' }, null);
+// console.log(result);
+
