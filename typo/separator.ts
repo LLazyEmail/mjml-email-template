6ac7420afde9ac9c274898b5b2116f21ddd2a78c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2025-08-09 10:30:13 +0300

    Update separator.ts

--- typo/separator.ts
+++ typo/separator.ts
@@ -10,3 +10,16 @@ const Separator = (attrs) => {
   const style = attrs && attrs.style ? attrs.style : "border:none; border-top:1px solid #ccc; margin:20px 0;";
   return `<hr style="${style}">`;
 };
+
+
+3. separator.ts
+ts
+Copy
+import { Tag } from "../types";
+
+const Separator: Tag = (attrs) => {
+  const style = attrs && attrs.style ? attrs.style : "border:none; border-top:1px solid #ccc; margin:20px 0;";
+  return `<hr style="${style}">`;
+};
+
+export default Separator;
