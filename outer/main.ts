f2e28af05ed9b4cff5961344d5f401c785e53ac7
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2025-08-09 10:03:23 +0300

    Update main.ts

--- outer/main.ts
+++ outer/main.ts
@@ -2,3 +2,27 @@
 
 
 </mjml>';
+
+
+
+
+
+// ts
+// import { Tag } from "../types";
+
+// const Main: Tag = (attrs, children) => {
+//   const { style } } = attrs || {};
+//   const styleAttr = style ?? "margin:20px auto; max-width:600px;";
+//   return `<main style="${styleAttr}">${children}</main>`;
+// };
+
+// export default Main;
+
+
+
+// js
+
+// const Main = (attrs, children) => {
+//   const styleAttr = attrs && attrs.style ? attrs.style : "margin:20px auto; max-width:600px;";
+//   return `<main style="${styleAttr}">${children}</main>`;
+// };
