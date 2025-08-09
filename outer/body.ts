737dc1bd53babd9b2ffdedbe50d722290d685233
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2025-08-09 10:01:08 +0300

    Update body.ts

--- outer/body.ts
+++ outer/body.ts
@@ -1,3 +1,28 @@
 '<mj-body>
 
 </mj-body>';
+
+
+
+
+
+// ts
+
+// import { Tag } from "../types";
+
+// const Body: Tag = (attrs, children) => {
+//   const { style } } = attrs || {};
+//   const styleAttr = style ?? "margin:0; padding:0;";
+//   return `<body style="${styleAttr}">${children}</body>`;
+// };
+
+// export default Body;
+
+
+// js
+
+// const Body = (attrs, children) => {
+//   const styleAttr = attrs && attrs.style ? attrs.style : "margin:0; padding:0;";
+//   return `<body style="${styleAttr}">${children}</body>`;
+// };
+
